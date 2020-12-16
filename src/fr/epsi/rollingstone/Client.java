7ae111ca70bb5e04d368dd199f967ff8f4be28bc
AlexiaTdev <alexia.teissier1@epsi.fr>
2020-12-16 11:45:20 +0100

    clientBean

---
+++ src/fr/epsi/rollingstone/Client.java
@@ -0,0 +1,41 @@
+package fr.epsi.rollingstone;
+
+public class Client {
+	private int id;
+	private String nom;
+	private int permis;
+	private int listidLocations;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public int getPermis() {
+		return permis;
+	}
+
+	public void setPermis(int permis) {
+		this.permis = permis;
+	}
+
+	public int getListidLocations() {
+		return listidLocations;
+	}
+
+	public void setListidLocations(int listidLocations) {
+		this.listidLocations = listidLocations;
+	}
+
+}
