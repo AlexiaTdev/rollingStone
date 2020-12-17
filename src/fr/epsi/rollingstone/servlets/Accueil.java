78a5539691435de1ac7a464b0e7038ffb019024b
AlexiaTdev <alexia.teissier1@epsi.fr>
2020-12-17 10:08:48 +0100

    modif voitures

--- src/fr/epsi/rollingstone/servlets/Accueil.java
+++ src/fr/epsi/rollingstone/servlets/Accueil.java
@@ -15,7 +15,8 @@ import fr.epsi.rollingstone.beans.Voiture;
 @WebServlet("/Accueil")
 public class Accueil extends HttpServlet {
 	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		Voiture renault5 = new Voiture();
 		renault5.setCouleur("rouge");
 		renault5.setEnergie("diesel");
@@ -28,7 +29,7 @@ public class Accueil extends HttpServlet {
 		renault5.setPuissanceChevaux(20);
 		Voiture peugeot206 = new Voiture();
 		peugeot206.setCouleur("bleu");
-		peugeot206.setEnergie("escence");
+		peugeot206.setEnergie("essence");
 		peugeot206.setPrixParJour(14.9);
 		peugeot206.setCaution(500);
 		peugeot206.setMarque("Peugeot");
@@ -38,7 +39,7 @@ public class Accueil extends HttpServlet {
 		peugeot206.setPuissanceChevaux(40);
 		Voiture fordfocus = new Voiture();
 		fordfocus.setCouleur("prune");
-		fordfocus.setEnergie("escence");
+		fordfocus.setEnergie("essence");
 		fordfocus.setPrixParJour(16);
 		fordfocus.setCaution(600);
 		fordfocus.setMarque("Ford");
@@ -46,10 +47,33 @@ public class Accueil extends HttpServlet {
 		fordfocus.setNbPlaces(4);
 		fordfocus.setPlaque("DPA 40 EF");
 		fordfocus.setPuissanceChevaux(75);
+		Voiture bmwz3 = new Voiture();
+		bmwz3.setCouleur("gris");
+		bmwz3.setEnergie("essence");
+		bmwz3.setPrixParJour(56);
+		bmwz3.setCaution(900);
+		bmwz3.setMarque("BMW");
+		bmwz3.setModel("z3");
+		bmwz3.setNbPlaces(2);
+		bmwz3.setPlaque("566 20 AT");
+		bmwz3.setPuissanceChevaux(170);
+		Voiture teslaX = new Voiture();
+		teslaX.setCouleur("gris");
+		teslaX.setEnergie("electrique");
+		teslaX.setPrixParJour(50);
+		teslaX.setCaution(900);
+		teslaX.setMarque("Tesla");
+		teslaX.setModel("modele X");
+		teslaX.setNbPlaces(7);
+		teslaX.setPlaque("655 21 AT");
+		teslaX.setPuissanceChevaux(170);
+
 		ArrayList<Voiture> listeVoiture = new ArrayList<>();
 		listeVoiture.add(renault5);
 		listeVoiture.add(peugeot206);
 		listeVoiture.add(fordfocus);
+		listeVoiture.add(bmwz3);
+		listeVoiture.add(teslaX);
 		request.getSession().setAttribute("Voitures", listeVoiture);
 		this.getServletContext().getRequestDispatcher("/WEB-INF/jsp/accueil.jsp").forward(request, response);
 	}
